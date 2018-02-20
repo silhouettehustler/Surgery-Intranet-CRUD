91a44beb33d126fc0071309582709b08f6f5484a
MatejaG <matejaglusac1993@gmail.com>
2018-02-20 11:18:35 +0000

    Added layout and node dependencies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,17 @@
-<h1>Welcome!</h1>
+@extends('layouts.app')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel">
+            <div class="panel-heading">
+                <h3>Login</h3>
+            </div>
+            <div class="panel-body">
+                <form>
+                    <input type="text">
+                    <input type="text">
+                    <input type="button" class="btn btn-sm btn-primary" value="Login">
+            </div>
+        </div>
+    </div>
+</div>
