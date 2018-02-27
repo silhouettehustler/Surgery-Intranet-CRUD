3752aced5120a07e78eb9f6cb2dc58ec21af170f
MatejaG <matejaglusac1993@gmail.com>
2018-02-27 22:01:59 +0000

    Added dashboard icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,22 @@
                             {{ session('status') }}
                         </div>
                     @else
-                    You are logged in!
+                    <div class="row">
+                        <div class="col-md-12" style="display: -webkit-box;text-align: center;">
+                            <div class="col-md-4">
+                                <img class="dashboard-card-img col-md-12" src="{{asset('images/appointment-img.jpeg')}}" alt="Appointments">
+                                <i class="col-md-12">APPOINTMENTS</i>
+                            </div>
+                            <div class="col-md-4">
+                                <img class="dashboard-card-img col-md-12" src="{{asset('images/results-img.jpeg')}}" alt="Results">
+                                <i class="col-md-12">RESULTS</i>
+                            </div>
+                            <div class="col-md-4">
+                                <img class="dashboard-card-img col-md-12" src="{{asset('images/help-img.jpeg')}}" alt="Online Chat">
+                                <i class="col-md-12">ONLINE CHAT</i>
+                            </div>
+                        </div>
+                    </div>
                     @endif
                 </div>
             </div>
@@ -21,3 +36,14 @@
     </div>
 </div>
 @endsection
+
+<style>
+    .dashboard-card-img{
+        border: 1px solid #848496;
+        cursor: pointer;
+        background: #d5fff1
+    }
+    .dashboard-card-img:hover{
+        background-color: #8897ff;
+    }
+</style>
